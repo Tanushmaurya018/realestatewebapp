b03ace73af22d956e6f26cc0c0020b707591eed9
Tanush Maurya <tanushmaurya018@gmail.com>
2024-01-18 14:54:28 +0530

    build fix 6

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use("/api/user",userRouter)
 app.use("/api/listing",listingRouter)
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
